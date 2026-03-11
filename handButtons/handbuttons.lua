7ddfa4f5f58b686e7f5d61612fe9cebdfb59ede0
111100001 <fqf1fqf@gmail.com>
2026-03-12 02:37:19 +0300

    Adjust minimum height for hand button in create_UIBox_buttons function

--- handButtons/handbuttons.lua
+++ handButtons/handbuttons.lua
@@ -9,7 +9,7 @@ local create_UIBox_buttonsRef = create_UIBox_buttons
 function create_UIBox_buttons()
     content = create_UIBox_buttonsRef()
     local hand_button = UIBox_button({
-        minh = 1,
+        minh = 0.5,
         minw = 1,
         button = "play_flush",
         label = {
