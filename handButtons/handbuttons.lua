323e28db7d11404fe1f39603dac079b6cf7609e7
111100001 <fqf1fqf@gmail.com>
2026-03-11 18:16:10 +0300

    Add initial .gitignore, .luarc.json, handbuttons.lua, and metadata.json files for hand buttons UI

---
+++ handButtons/handbuttons.lua
@@ -0,0 +1,22 @@
+
+function G.FUNCS.play_flush()
+    -- implement flush logic
+end
+
+
+
+local create_UIBox_buttonsRef = create_UIBox_buttons
+function create_UIBox_buttons()
+    content = create_UIBox_buttonsRef()
+    local hand_button = UIBox_button({
+        minh = 1,
+        minw = 1,
+        button = "play_flush",
+        label = {
+            "Flush"
+        }
+    })
+    table.insert(content.nodes, hand_button)
+    return content
+end
+
